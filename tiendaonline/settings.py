18e91681357230f27bd7fc471fe74dd2ca291dd8
Luca Citta <lucacitta@gmail.com>
2021-08-01 17:03:59 +0200

    'CambioSqlite'

--- tiendaonline/settings.py
+++ tiendaonline/settings.py
@@ -85,13 +85,13 @@ WSGI_APPLICATION = 'tiendaonline.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-""" DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-} """
-
+}
+'''
 import dj_database_url
 from decouple import config
 
@@ -100,7 +100,7 @@ DATABASES = {
         default=config('DATABASE_URL')
     )
 }
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
