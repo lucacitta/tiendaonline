d4616669c297a1c46365e95eced428cf97a69511
Luca Citta <lucacitta@gmail.com>
2021-08-07 21:33:54 +0200

    PaginaTerminada1.0

--- blog/views.py
+++ blog/views.py
@@ -4,9 +4,11 @@ from .models import Categoria, Post
 
 def blog(request):
     posts=Post.objects.all()
-    return render(request, 'blog/blog.html', {'posts':posts})
+    categorias=Categoria.objects.all()
+    return render(request, 'blog/blog.html', {'posts':posts,'categorias':categorias})
 
 def categoria(request, categoria_id):
     categoria=Categoria.objects.get(id=categoria_id)
     posts=Post.objects.filter(categorias=categoria)
-    return render(request, 'blog/categorias.html', {'posts':posts})
+    print (categoria)
+    return render(request, 'blog/categorias.html', {'posts':posts,'categoria':categoria})
--- contacto/forms.py
+++ contacto/forms.py
@@ -1,7 +1,10 @@
 from django import forms
-from django.forms.widgets import Textarea
+from django.forms import fields
+from django.forms import widgets
+from django.forms.widgets import Textarea, Widget
 
 class FormularioContacto(forms.Form):
-    nombre=forms.CharField(label='Ingresa tu nombre', max_length=100, required=True)
-    email=forms.EmailField(label='Ingresa tu email', max_length=100, required=True)
-    contenido=forms.CharField(label='Ingresa tu consulta', max_length=200, required=True, widget=forms.Textarea)
+    nombre=forms.CharField(label='Ingresa tu nombre', max_length=100, required=True, widget=forms.TextInput(attrs={'class':'form-control'}))
+    email=forms.EmailField(label='Ingresa tu email', max_length=100, required=True, widget=forms.TextInput(attrs={'class':'form-control'}))
+    contenido=forms.CharField(label='Ingresa tu consulta', max_length=200, required=True, widget=forms.Textarea(attrs={'class':'form-control'}))
+
