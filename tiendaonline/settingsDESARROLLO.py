70e3b32a49b62e1bf6a9ab30ae5d075e2a490be6
Luca Citta <lucacitta@gmail.com>
2021-10-12 12:02:20 +0200

    "Cambios_seg"

--- tiendaonline/settingsDESARROLLO.py
+++ tiendaonline/settingsDESARROLLO.py
@@ -145,8 +145,8 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
-EMAIL_HOST_USER='lucacitta.python@gmail.com'
-EMAIL_HOST_PASSWORD='Mymamapython12'
+EMAIL_HOST_USER='' #EMAIL DE TU CUENTA
+EMAIL_HOST_PASSWORD='' #CONTRASEÑA DE TU CUENTA
 
 # accounts
 
